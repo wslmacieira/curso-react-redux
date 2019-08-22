b1cfa4a5bc8284b699c69e6ad28b05da6c72f8d7
wslmacieira <wslmacieira@outlook.com>
2019-08-22 13:09:39 -0300

    projeto todo-app concluido

--- todo-app/frontend/src/about/about.jsx
+++ todo-app/frontend/src/about/about.jsx
@@ -1,7 +1,9 @@
 import React from 'react' 
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+         <PageHeader name='Sobre' small='Nós' />
+         <h2>Nossa História</h2>
     </div>
 )
