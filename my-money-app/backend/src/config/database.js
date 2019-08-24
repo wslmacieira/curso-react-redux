3552ddceaa262f32c2c4b0dc50a3497718fce243
wslmacieira <wslmacieira@outlook.com>
2019-08-23 21:05:22 -0300

    backend finalizado

--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true })
 
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -3,9 +3,13 @@ const port = 3003
 const bodyparser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyparser.urlencoded({ extended: true}))
 server.use(bodyparser.json())
+server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}`)
