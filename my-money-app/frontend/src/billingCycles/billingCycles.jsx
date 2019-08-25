5935e09bf28bff5ed26885e8011114088449a1fc
wslmacieira <wslmacieira@outlook.com>
2019-08-24 23:50:50 -0300

    reduxForm e msgs de errors e success

--- my-money-app/frontend/src/billingCycles/billingCycles.jsx
+++ my-money-app/frontend/src/billingCycles/billingCycles.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-export default props => (
-    <div>
-        <h1>Ciclo de Pagamento</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabsContent from '../common/tab/tabsContent'
+import TabHeader from '../common/tab/tabHeader'
+import TabContent from '../common/tab/tabContent'
+import { selectTab, showTabs } from '../common/tab/tabActions'
+import { create } from './billingCyclesActions'
+
+import List from '../billingCycles/billingCycleList'
+import Form from '../billingCycles/billingCycleForm'
+
+
+class BillingCycle extends Component {
+
+    componentWillMount() {
+        this.props.selectTab('tabList')
+        this.props.showTabs('tabList', 'tabCreate')
+    }
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+                            <TabContent id='tabList'>
+                                <List />
+                            </TabContent>
+                            <TabContent id='tabCreate'>
+                                <Form onSubmit={this.props.create}/>
+                            </TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ selectTab, showTabs,  create }, dispatch);
+export default connect(null, mapDispatchToProps)(BillingCycle)
--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,8 +1,17 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import { reducer as toastReducer } from 'react-redux-toastr'
+
 import DashboarReducer from '../dashboard/dashboardReducer'
+import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycles/billingCycleReducer'
 
 const rootReducer = combineReducers({
-    dashboard: DashboarReducer
+    dashboard: DashboarReducer,
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer,
+    form: formReducer,
+    toastr: toastReducer
 })
 
 export default rootReducer
