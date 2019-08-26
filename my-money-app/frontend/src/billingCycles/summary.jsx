632c9fa7f084ffba92f740b1199c0d1ea46a39a0
wslmacieira <wslmacieira@outlook.com>
2019-08-26 00:04:46 -0300

    navegação com React-router (Versão 2)

--- my-money-app/frontend/src/billingCycles/summary.jsx
+++ my-money-app/frontend/src/billingCycles/summary.jsx
@@ -11,9 +11,9 @@ export default ( {credit, debt}) => (
             <Row>
                 <ValueBox cols='12 4' color='green' icon='bank'
                     value={`R$ ${credit}`} text='Total de Créditos' />
-                <ValueBox cols='12 4' color='red' icon='bank'
+                <ValueBox cols='12 4' color='red' icon='credit-card'
                     value={`R$ ${debt}`} text='Total de Débitos' />
-                <ValueBox cols='12 4' color='blue' icon='bank'
+                <ValueBox cols='12 4' color='blue' icon='money'
                     value={`R$ ${credit - debt}`} text='Valor Consolidado' />
             </Row>
         </fieldset>
--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Router, Route, Redirect, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
+import App from './app'
 import Dashboard from '../dashboard2/dashboard2'
 import BillingCycles from '../billingCycles/billingCycles'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
-        <Route path='/billingCycles' component={BillingCycles} />
+        <Route path='/' component={App}>
+            <IndexRoute component={Dashboard} />
+            <Route path='billingCycles' component={BillingCycles} />
+        </Route>
         <Redirect from='*' to='/' />
     </Router>
 )
